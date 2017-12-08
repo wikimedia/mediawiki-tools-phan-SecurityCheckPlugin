955f7bffb713973d9a0a544cafc089df54744c05
Brian Wolff <bawolff+wn@gmail.com>
2017-12-08 19:01:37 +0000

    Add GPL license headers
    Change-Id: Ie9106c80b23f23a393912a12c9368078bcdb3557

--- GenericSecurityCheckPlugin.php
+++ GenericSecurityCheckPlugin.php
@@ -1,18 +1,34 @@
 <?php
-require_once __DIR__ . "/src/SecurityCheckPlugin.php";
-
 /**
- * Copyright Brian Wolff 2017. Released under the GPL version 2 or later.
- *
- * This is a generic security check plugin.
+ * This is phan plugin to provide security static analysis checks for php
  *
  * If your project has functions/methods whose output you
  * specificly need to mark tainted, then you probably
  * want to make your own subcalss of SecurityCheckPlugin
  * and override getCustomFuncTaint().
  *
- * See MediaWikiSecurityCheckPlugin for an example.
+ * See MediaWikiSecurityCheckPlugin for an example of that.
+ *
+ * To use, add this file to the list of your phan plugins.
+ *
+ * Copyright (C) 2017  Brian Wolff <bawolff@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+require_once __DIR__ . "/src/SecurityCheckPlugin.php";
+
 class GenericSecurityCheckPlugin extends SecurityCheckPlugin {
 
 	/**
--- src/TaintednessBaseVisitor.php
+++ src/TaintednessBaseVisitor.php
@@ -30,6 +30,22 @@ use Phan\Exception\IssueException;
 /**
  * Base class for the Tainedness visitor subclass. Mostly contains
  * utility methods.
+ *
+ * Copyright (C) 2017  Brian Wolff <bawolff@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 abstract class TaintednessBaseVisitor extends AnalysisVisitor {
 
