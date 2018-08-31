70b4fd46aa62b1b535e4cf2a5a092e58afb30938
Brian Wolff <bawolff+wn@gmail.com>
2018-08-31 09:44:37 +0000

    Make onlysafefor_html not mark things as exec_escaped.
    We want exec_escaped in the @param-taint case, but not in the
    @return-taint case. So make it be set for escapes_html but
    not onlysafefor_html.
    
    This should reduce false positives related to log formatter
    
    Bug: T201565
    Change-Id: I2f4e33656b9f94bf82c55221473786d5b3483742

--- src/SecurityCheckPlugin.php
+++ src/SecurityCheckPlugin.php
@@ -237,7 +237,9 @@ abstract class SecurityCheckPlugin extends PluginImplementation {
 	 *     Note: escapes_html adds the exec_escaped flag, use
 	 *     escapes_htmlnoent if the value is safe to double encode.
 	 *  * onlysafefor_{type}
-	 *     Same as above, intended for @return statements. No special case htmlnoent.
+	 *     Same as above, intended for @return statements.
+	 *     Only difference is that onlysafefor_html sets ESCAPED_TAINT instead
+	 *     of ESCAPED_EXEC_TAINT
 	 *  * none - self::NO_TAINT
 	 *  * tainted - self::YES_TAINT
 	 *  * array_ok - sets self::ARRAY_OK
@@ -288,7 +290,11 @@ abstract class SecurityCheckPlugin extends PluginImplementation {
 				case 'onlysafefor':
 					$overallTaint |= ( self::YES_TAINT & ~$taintAsInt );
 					if ( $taintParts['type'] === 'html' ) {
-						$overallTaint |= self::ESCAPED_EXEC_TAINT;
+						if ( $taintParts['prefix'] === 'escapes' ) {
+							$overallTaint |= self::ESCAPED_EXEC_TAINT;
+						} else {
+							$overallTaint |= self::ESCAPED_TAINT;
+						}
 					}
 					break;
 			}
