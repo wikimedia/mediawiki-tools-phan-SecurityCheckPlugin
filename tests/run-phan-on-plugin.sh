cf6ee99a1acb02b0bfb98a36e308bfd1f8a94c8c
Brian Wolff <bawolff+wn@gmail.com>
2018-08-31 09:57:22 +0000

    Fix error in argument handling in test script
    Change-Id: I768df6fd6775d52c54df0a003728789b7e03dca7

--- tests/run-phan-on-plugin.sh
+++ tests/run-phan-on-plugin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dir=`dirname $0`
 cd "$dir/../"
-progressBar="$@"
+progressBar=""
 # This doesn't really work from composer
 if [ -t 2 ]
-	then progressBar="-p" "$@"
+	then progressBar="-p"
 fi
 
 echo Starting phan
@@ -13,7 +13,7 @@ php7.0 "./vendor/phan/phan/phan" \
         --project-root-directory . \
         --config-file "tests/general-phan-config.php" \
         --output "php://stdout" \
-        $progressBar
+        $progressBar $@
 result=$?
 echo
 exit $result
