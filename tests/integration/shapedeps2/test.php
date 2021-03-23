3216ce72b20feb693e744eb747d57b95b976b4ed
Daimona Eaytoy <daimona.wiki@gmail.com>
2021-03-23 18:03:06 +0100

    Add tests for shaped dependencies
    These only serve as a baseline for now. Test results are still far from
    what's really expected, but this will improve shortly.
    
    Change-Id: I187255ceaa44040cc3bbe44f49f136db06435acf

---
+++ tests/integration/shapedeps2/test.php
@@ -0,0 +1,351 @@
+<?php
+
+// Naming scheme: Test<where we put $arg><what is echoed>
+
+function createDependencyGraph() {
+	( new TestWholeWhole( 'foo' ) )->testWhole();
+	( new TestWholeSafe( 'foo' ) )->testSafe();
+	( new TestWholeUnsafe( 'foo' ) )->testUnsafe();
+	( new TestWholeUnknown( 'foo' ) )->testUnknown();
+	( new TestWholeKeys( 'foo' ) )->testKeys();
+	( new TestSafeWhole( 'foo' ) )->testWhole();
+	( new TestSafeSafe( 'foo' ) )->testSafe();
+	( new TestSafeUnsafe( 'foo' ) )->testUnsafe();
+	( new TestSafeUnknown( 'foo' ) )->testUnknown();
+	( new TestSafeKeys( 'foo' ) )->testKeys();
+	( new TestUnsafeWhole( 'foo' ) )->testWhole();
+	( new TestUnsafeSafe( 'foo' ) )->testSafe();
+	( new TestUnsafeUnsafe( 'foo' ) )->testUnsafe();
+	( new TestUnsafeUnknown( 'foo' ) )->testUnknown();
+	( new TestUnsafeKeys( 'foo' ) )->testKeys();
+	( new TestUnknownWhole( 'foo' ) )->testWhole();
+	( new TestUnknownSafe( 'foo' ) )->testSafe();
+	( new TestUnknownUnsafe( 'foo' ) )->testUnsafe();
+	( new TestUnknownUnknown( 'foo' ) )->testUnknown();
+	( new TestUnknownKeys( 'foo' ) )->testKeys();
+	( new TestKeysWhole( 'foo' ) )->testWhole();
+	( new TestKeysSafe( 'foo' ) )->testSafe();
+	( new TestKeysUnsafe( 'foo' ) )->testUnsafe();
+	( new TestKeysUnknown( 'foo' ) )->testUnknown();
+	( new TestKeysKeys( 'foo' ) )->testKeys();
+}
+
+function doEvilStuff() {
+	new TestWholeWhole( $_GET['x'] ); // Unsafe
+	new TestWholeSafe( $_GET['x'] ); // Unsafe
+	new TestWholeUnsafe( $_GET['x'] ); // Unsafe
+	new TestWholeUnknown( $_GET['x'] ); // Unsafe
+	new TestWholeKeys( $_GET['x'] ); // Unsafe
+	new TestSafeWhole( $_GET['x'] ); // Unsafe
+	new TestSafeSafe( $_GET['x'] ); // Unsafe
+	new TestSafeUnsafe( $_GET['x'] ); // Unsafe
+	new TestSafeUnknown( $_GET['x'] ); // Unsafe
+	new TestSafeKeys( $_GET['x'] ); // TODO Safe
+	new TestUnsafeWhole( $_GET['x'] ); // Unsafe
+	new TestUnsafeSafe( $_GET['x'] ); // TODO Safe
+	new TestUnsafeUnsafe( $_GET['x'] ); // Unsafe
+	new TestUnsafeUnknown( $_GET['x'] ); // Unsafe
+	new TestUnsafeKeys( $_GET['x'] ); // TODO Safe
+	new TestUnknownWhole( $_GET['x'] ); // Unsafe
+	new TestUnknownSafe( $_GET['x'] ); // Unsafe
+	new TestUnknownUnsafe( $_GET['x'] ); // Unsafe
+	new TestUnknownUnknown( $_GET['x'] ); // Unsafe
+	new TestUnknownKeys( $_GET['x'] ); // TODO Safe
+	new TestKeysWhole( $_GET['x'] ); // Unsafe
+	new TestKeysSafe( $_GET['x'] ); // TODO Safe
+	new TestKeysUnsafe( $_GET['x'] ); // TODO Safe
+	new TestKeysUnknown( $_GET['x'] ); // TODO Safe
+	new TestKeysKeys( $_GET['x'] ); // Unsafe
+}
+
+class TestWholeWhole {
+	public $propWholeWhole;
+
+	public function testWhole() {
+		echoWhole( $this->propWholeWhole ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propWholeWhole = $arg;
+	}
+}
+class TestWholeSafe {
+	public $propWholeSafe;
+
+	public function testSafe() {
+		echoSafe( $this->propWholeSafe ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propWholeSafe = $arg;
+	}
+}
+class TestWholeUnsafe {
+	public $propWholeUnsafe;
+
+	public function testUnsafe() {
+		echoUnsafe( $this->propWholeUnsafe ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propWholeUnsafe = $arg;
+	}
+}
+class TestWholeUnknown {
+	public $propWholeUnknown;
+
+	public function testUnknown() {
+		echoUnknown( $this->propWholeUnknown ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propWholeUnknown = $arg;
+	}
+}
+class TestWholeKeys {
+	public $propWholeKeys;
+
+	public function testKeys() {
+		echoKeys( $this->propWholeKeys ); // Unsafe
+	}
+
+	public function __construct( $arg ) {
+		$this->propWholeKeys = $arg;
+	}
+}
+
+class TestSafeWhole {
+	public $propSafeWhole;
+
+	public function testWhole() {
+		echoWhole( $this->propSafeWhole ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propSafeWhole = [ 'safe' => $arg, 'unsafe' => $_GET['a'] ];
+	}
+}
+class TestSafeSafe {
+	public $propSafeSafe;
+
+	public function testSafe() {
+		echoSafe( $this->propSafeSafe ); // TODO Safe
+	}
+	public function __construct( $arg ) {
+		$this->propSafeSafe = [ 'safe' => $arg, 'unsafe' => $_GET['a'] ];
+	}
+}
+class TestSafeUnsafe {
+	public $propSafeUnsafe;
+
+	public function testUnsafe() {
+		echoUnsafe( $this->propSafeUnsafe ); // TODO Safe
+	}
+	public function __construct( $arg ) {
+		$this->propSafeUnsafe = [ 'safe' => $arg, 'unsafe' => $_GET['a'] ];
+	}
+}
+class TestSafeUnknown {
+	public $propSafeUnknown;
+
+	public function testUnknown() {
+		echoUnknown( $this->propSafeUnknown ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propSafeUnknown = [ 'safe' => $arg, 'unsafe' => $_GET['a'] ];
+	}
+}
+class TestSafeKeys {
+	public $propSafeKeys;
+
+	public function testKeys() {
+		echoKeys( $this->propSafeKeys ); // TODO Safe
+	}
+
+	public function __construct( $arg ) {
+		$this->propSafeKeys = [ 'safe' => $arg, 'unsafe' => $_GET['a'] ];
+	}
+}
+
+class TestUnsafeWhole {
+	public $propUnsafeWhole;
+
+	public function testWhole() {
+		echoWhole( $this->propUnsafeWhole ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnsafeWhole = [ 'unsafe' => $arg, 'safe' => 'safe' ];
+	}
+}
+class TestUnsafeSafe {
+	public $propUnsafeSafe;
+
+	public function testSafe() {
+		echoSafe( $this->propUnsafeSafe ); // TODO Safe
+	}
+	public function __construct( $arg ) {
+		$this->propUnsafeSafe = [ 'unsafe' => $arg, 'safe' => 'safe' ];
+	}
+}
+class TestUnsafeUnsafe {
+	public $propUnsafeUnsafe;
+
+	public function testUnsafe() {
+		echoUnsafe( $this->propUnsafeUnsafe ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnsafeUnsafe = [ 'unsafe' => $arg, 'safe' => 'safe' ];
+	}
+}
+class TestUnsafeUnknown {
+	public $propUnsafeUnknown;
+
+	public function testUnknown() {
+		echoUnknown( $this->propUnsafeUnknown ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnsafeUnknown = [ 'unsafe' => $arg, 'safe' => 'safe' ];
+	}
+}
+class TestUnsafeKeys {
+	public $propUnsafeKeys;
+
+	public function testKeys() {
+		echoKeys( $this->propUnsafeKeys ); // TODO Safe
+	}
+
+	public function __construct( $arg ) {
+		$this->propUnsafeKeys = [ 'unsafe' => $arg, 'safe' => 'safe' ];
+	}
+}
+
+class TestUnknownWhole {
+	public $propUnknownWhole;
+
+	public function testWhole() {
+		echoWhole( $this->propUnknownWhole ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnknownWhole = [ getUnknown() => $arg ];
+	}
+}
+class TestUnknownSafe {
+	public $propUnknownSafe;
+
+	public function testSafe() {
+		echoSafe( $this->propUnknownSafe ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnknownSafe = [ getUnknown() => $arg ];
+	}
+}
+class TestUnknownUnsafe {
+	public $propUnknownUnsafe;
+
+	public function testUnsafe() {
+		echoUnsafe( $this->propUnknownUnsafe ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnknownUnsafe = [ getUnknown() => $arg ];
+	}
+}
+class TestUnknownUnknown {
+	public $propUnknownUnknown;
+
+	public function testUnknown() {
+		echoUnknown( $this->propUnknownUnknown ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propUnknownUnknown = [ getUnknown() => $arg ];
+	}
+}
+class TestUnknownKeys {
+	public $propUnknownKeys;
+
+	public function testKeys() {
+		echoKeys( $this->propUnknownKeys ); // TODO Safe
+	}
+
+	public function __construct( $arg ) {
+		$this->propUnknownKeys = [ getUnknown() => $arg ];
+	}
+}
+
+class TestKeysWhole {
+	public $propKeysWhole;
+
+	public function testWhole() {
+		echoWhole( $this->propKeysWhole ); // Unsafe
+	}
+	public function __construct( $arg ) {
+		$this->propKeysWhole = [ $arg => 'safe' ];
+	}
+}
+class TestKeysSafe {
+	public $propKeysSafe;
+
+	public function testSafe() {
+		echoSafe( $this->propKeysSafe ); // TODO Safe
+	}
+	public function __construct( $arg ) {
+		$this->propKeysSafe = [ $arg => 'safe' ];
+	}
+}
+class TestKeysUnsafe {
+	public $propKeysUnsafe;
+
+	public function testUnsafe() {
+		echoUnsafe( $this->propKeysUnsafe ); // TODO Safe
+	}
+	public function __construct( $arg ) {
+		$this->propKeysUnsafe = [ $arg => 'safe' ];
+	}
+}
+class TestKeysUnknown {
+	public $propKeysUnknown;
+
+	public function testUnknown() {
+		echoUnknown( $this->propKeysUnknown ); // TODO Safe
+	}
+	public function __construct( $arg ) {
+		$this->propKeysUnknown = [ $arg => 'safe' ];
+	}
+}
+class TestKeysKeys {
+	public $propKeysKeys;
+
+	public function testKeys() {
+		echoKeys( $this->propKeysKeys ); // Unsafe
+	}
+
+	public function __construct( $arg ) {
+		$this->propKeysKeys = [ $arg => 'safe' ];
+	}
+}
+
+
+
+function echoWhole( $arg ) {
+	echo $arg;
+}
+
+function echoSafe( $arg ) {
+	echo $arg['safe'];
+}
+
+function echoUnsafe( $arg ) {
+	echo $arg['unsafe'];
+}
+
+function echoUnknown( $arg ) {
+	echo $arg[getUnknown()];
+}
+
+function echoKeys( $arg ) {
+	foreach ( $arg as $k => $_ ) {
+		echo $k;
+	}
+}
+
+/**
+ * Helper to get an unknown type but without taint
+ * @return-taint none
+ */
+function getUnknown() {
+	return $GLOBALS['unknown'];
+}
---
+++ tests/integration/shapedeps3/test.php
@@ -0,0 +1,136 @@
+<?php
+
+function createDependencyGraph() {
+	( new TestWhole( 'foo' ) )->doTest();
+	( new TestEcho( 'foo' ) )->doTest();
+	( new TestShell( 'foo' ) )->doTest();
+	( new TestRequireWhole( 'foo' ) )->doTest();
+	( new TestRequireSafe( 'foo' ) )->doTest();
+	( new TestRequireUnsafe( 'foo' ) )->doTest();
+	( new TestUnknown( 'foo' ) )->doTest();
+	( new TestKeys( 'foo' ) )->doTest();
+}
+
+function doEvilStuff() {
+	new TestWhole( $_GET['a'] ); // HTML, SHELL, PATH, SERIALIZE
+	new TestEcho( $_GET['a'] ); // HTML
+	new TestShell( $_GET['a'] ); // SHELL
+	new TestRequireWhole( $_GET['a'] ); // PATH
+	new TestRequireSafe( $_GET['a'] ); // Safe
+	new TestRequireUnsafe( $_GET['a'] ); // PATH
+	new TestUnknown( $_GET['a'] ); // HTML, SHELL, PATH
+	new TestKeys( $_GET['a'] ); // SERIALIZE
+}
+
+class TestWhole {
+	public $wholeProp;
+
+	public function doTest() {
+		allSink( $this->wholeProp ); // HTML, SHELL, PATH, SERIALIZE
+	}
+
+	public function __construct( $arg ) {
+		$this->wholeProp = $arg;
+	}
+}
+
+class TestEcho {
+	public $echoProp;
+
+	public function doTest() {
+		allSink( $this->echoProp ); // HTML
+	}
+
+	public function __construct( $arg ) {
+		$this->echoProp = [ 'echo' => $arg, 'shell' => 'safe', 'require' => [] ];
+	}
+}
+
+class TestShell {
+	public $shellProp;
+
+	public function doTest() {
+		allSink( $this->shellProp ); // SHELL
+	}
+
+	public function __construct( $arg ) {
+		$this->shellProp = [ 'echo' => 'safe', 'shell' => $arg, 'require' => [] ];
+	}
+}
+
+class TestRequireWhole {
+	public $requireWholeProp;
+
+	public function doTest() {
+		allSink( $this->requireWholeProp ); // PATH
+	}
+
+	public function __construct( $arg ) {
+		$this->requireWholeProp = [ 'echo' => 'safe', 'shell' => 'x', 'require' => $arg ];
+	}
+}
+
+class TestRequireSafe {
+	public $requireSafeProp;
+
+	public function doTest() {
+		allSink( $this->requireSafeProp ); // Safe
+	}
+
+	public function __construct( $arg ) {
+		$this->requireSafeProp = [ 'echo' => 'safe', 'shell' => 'x', 'require' => [ 'safe' => $arg, 'file' => 'safe' ] ];
+	}
+}
+
+class TestRequireUnsafe {
+	public $requireUnsafeProp;
+
+	public function doTest() {
+		allSink( $this->requireUnsafeProp );  // PATH
+	}
+
+	public function __construct( $arg ) {
+		$this->requireUnsafeProp = [ 'echo' => 'safe', 'shell' => 'x', 'require' => [ 'file' => $arg ] ];
+	}
+}
+
+class TestUnknown {
+	public $unknownProp;
+
+	public function doTest() {
+		allSink( $this->unknownProp ); // HTML, SHELL, PATH
+	}
+
+	public function __construct( $arg ) {
+		$this->unknownProp = [ getUnknown() => $arg ];
+	}
+}
+
+class TestKeys {
+	public $keysProp;
+
+	public function doTest() {
+		allSink( $this->keysProp ); // SERIALIZE
+	}
+
+	public function __construct( $arg ) {
+		$this->keysProp = [ $arg => 'safe' ];
+	}
+}
+
+function allSink( $arg ) {
+	echo $arg['echo'];
+	shell_exec( $arg['shell'] );
+	require $arg['require']['file'];
+	foreach ( $arg as $k => $v ) {
+		unserialize( $k );
+	}
+}
+
+/**
+ * Helper to get an unknown type but without taint
+ * @return-taint none
+ */
+function getUnknown() {
+	return $GLOBALS['unknown'];
+}
