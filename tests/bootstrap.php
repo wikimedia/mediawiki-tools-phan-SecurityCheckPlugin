92bf31553701e2b8d5e8027b124f548a7912aaf6
Daimona Eaytoy <daimona.wiki@gmail.com>
2021-04-18 12:58:21 +0200

    Really load SecurityCheckPlugin in the PHPUnit bootstrap file
    We need to load that class so that PHPUnit can backup $docblockCache.
    Failing to do so will result in random test failures: one that happens
    fairly often in WMF CI is a failure of the "backpropoffsets2" test if
    it's run after "shapedeps3".
    
    Change-Id: I13d4f9a393010dade339bb2fae7c3c346f8f1706

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
+use SecurityCheckPlugin\SecurityCheckPlugin;
+
 // We need to load this class now, so that when PHPUnit takes a snapshot of static attributes
 // it also includes GlobalScope::global_variable_map. Failing to back up this property (or backing
 // it up when it was already populated) will result in weird false positives.
 require_once __DIR__ . '/../vendor/phan/phan/src/Phan/Language/Scope/GlobalScope.php';
 // Same as above, for SecurityCheckPlugin::$docblockCache
 require_once __DIR__ . '/../vendor/autoload.php';
+class_exists( SecurityCheckPlugin::class );
