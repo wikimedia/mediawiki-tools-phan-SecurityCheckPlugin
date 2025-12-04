8b88e078470ccb4a96c295960498cdbc74383535
James D. Forrester <jforrester@wikimedia.org>
2025-12-04 17:04:19 -0500

    Update copy of GPL license and licensing headers in files
    Bug: T400642
    Change-Id: I1bf6aa77269748da445f723fe6c02892c54cec12

--- src/TaintednessBaseVisitor.php
+++ src/TaintednessBaseVisitor.php
@@ -1,5 +1,11 @@
 <?php declare( strict_types=1 );
 
+/**
+ * Copyright (C) 2017  Brian Wolff <bawolff@gmail.com>
+ *
+ * @license GPL-2.0-or-later
+ */
+
 namespace SecurityCheckPlugin;
 
 use ast\Node;
@@ -44,23 +50,6 @@ use const ast\AST_STATIC_CALL;
  * Trait for the Tainedness visitor subclasses. Mostly contains
  * utility methods.
  *
- * Copyright (C) 2017  Brian Wolff <bawolff@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-/**
  * @property-read Context $context
  * @property-read \Phan\CodeBase $code_base
  */
